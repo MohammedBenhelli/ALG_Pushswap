d65ef33686ae6683f234dc252a9768e40db24e12
mohammed <mohammed.benhelli@epitech.eu>
2020-07-26 15:44:42 +0200

    push

--- lib.php
+++ lib.php
@@ -2,59 +2,70 @@
 
 use Ds\Deque;
 
-function sa(Deque &$la): void {
+function sa(Deque &$la, string &$str, bool $t = true): void {
     if ($la->count() > 1){
         [$f, $l] = [$la->shift(), $la->pop()];
         $la->unshift($l);
         $la->push($f);
     }
+    if ($t) $str.="sa ";
 }
 
-function sb(Deque &$lb): void {
+function sb(Deque &$lb, string &$str, bool $t = true): void {
     if ($lb->count() > 1){
         [$f, $l] = [$lb->shift(), $lb->pop()];
         $lb->unshift($l);
         $lb->push($f);
     }
+    if ($t) $str.="sb ";
 }
 
-function sc(Deque &$la, Deque &$lb): void {
-    sa($la);
-    sb($lb);
+function sc(Deque &$la, Deque &$lb, string &$str): void {
+    sa($la, $str, false);
+    sb($lb, $str, false);
+    $str.="sc ";
 }
 
-function pa(Deque &$la, Deque &$lb): void {
+function pa(Deque &$la, Deque &$lb, string &$str): void {
     if($lb->count() > 0)
         $la->unshift($lb->shift());
+    $str.="pa ";
 }
 
-function pb(Deque &$la, Deque &$lb): void {
+function pb(Deque &$la, Deque &$lb, string &$str): void {
     if($la->count() > 0)
         $lb->unshift($la->shift());
+    $str.="pb ";
 }
 
-function ra(Deque &$la): void {
+function ra(Deque &$la, string &$str, bool $t = true): void {
     $la->push($la->shift());
+    if ($t) $str.="ra ";
 }
 
-function rb(Deque &$lb): void {
+function rb(Deque &$lb, string &$str, bool $t = true): void {
     $lb->push($lb->shift());
+    if ($t) $str.="rb ";
 }
 
-function rr(Deque &$la, Deque &$lb): void {
-    ra($la);
-    rb($lb);
+function rr(Deque &$la, Deque &$lb, string &$str): void {
+    ra($la, $str, false);
+    rb($lb, $str, false);
+    $str.="rr ";
 }
 
-function rra(Deque &$la): void {
+function rra(Deque &$la, string &$str, bool $t = true): void {
     $la->unshift($la->pop());
+    if ($t) $str.="rra ";
 }
 
-function rrb(Deque &$lb): void {
+function rrb(Deque &$lb, string &$str, bool $t = true): void {
     $lb->unshift($lb->pop());
+    if ($t) $str.="rrb ";
 }
 
-function rrr(Deque &$la, Deque &$lb): void {
-    rra($la);
-    rrb($lb);
+function rrr(Deque &$la, string &$str, Deque &$lb): void {
+    rra($la, $str, false);
+    rrb($lb,$str , false);
+    $str.="rrr ";
 }
--- test.php
+++ test.php
@@ -3,17 +3,48 @@
 use Ds\Deque;
 require "./lib.php";
 
+function sorted(Deque $l): bool{
+    for($i = 0; $i < $l->count() - 1; $i++)
+        if ($l[$i] > $l[$i + 1])
+            return false;
+    return true;
+}
+
+function minDeque(Deque $l): array{
+    $min = PHP_INT_MAX;
+    $offset = 0;
+    for($i = 0; $i < $l->count(); $i++)
+        if ($l[$i] < $min){
+            $min = $l[$i];
+            $offset = $i;
+        }
+    return [$min, $offset];
+}
+
+function sortDeque(Deque &$la, Deque &$lb): void{
+    $str = "";
+    while($la->count() > 0){
+        [$min, $offset] = minDeque($la);
+        if($la->count() / 2 > $offset)
+            while($offset-- > 0)
+                ra($la, $str);
+        else while($offset++ < $la->count())
+                rra($la, $str);
+        pb($la, $lb, $str);
+    }
+    while($lb->count() > 0)
+        pa($la, $lb, $str);
+    echo rtrim($str, " ")."\n";
+}
+
 function init(Deque &$la, Deque &$lb, array $argv): void {
     array_shift($argv);
     $la->push(...$argv);
+    if(!sorted($la))
+        sortDeque($la, $lb);
 }
 
 $la = new Deque();
 $lb = new Deque();
-$lb->push(15);
 
 init($la, $lb, $argv);
-rra($la);
-
-print_r($la);
-print_r($lb);
